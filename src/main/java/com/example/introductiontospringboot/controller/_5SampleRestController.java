485fe5bb1be7805b929efbd2582473c61b6f0f73
jyfernandez <jydfernandez@gmail.com>
2024-04-29 18:45:27 +0800

    Sample Cors

--- src/main/java/com/example/introductiontospringboot/controller/_5SampleRestController.java
+++ src/main/java/com/example/introductiontospringboot/controller/_5SampleRestController.java
@@ -1,6 +1,7 @@
 package com.example.introductiontospringboot.controller;
 
 import com.example.introductiontospringboot.model.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,16 @@ public class _5SampleRestController {
         User user = User.builder()
                 .first_name("Jy")
                 .last_name("Fernandez")
+                .build();
+        return user;
+    }
+
+    @GetMapping("/sample/cors")
+    @CrossOrigin(origins = "http://wwww.example.com", maxAge = 36000)
+    public User getUserCors (){
+        User user = User.builder()
+                .first_name("Sample")
+                .last_name("Cors")
                 .build();
         return user;
     }
